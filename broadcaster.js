502744d1e5759dc6b9da013695c081f9c93acbd9
malikarun <akmalik@ymail.com>
2015-11-15 11:12:40 +1000

    websocket server

--- broadcaster.js
+++ broadcaster.js
@@ -1,5 +1,5 @@
 window.onload = function(){
-  var client = new BinaryClient('wss://kasoot.herokuapp.com');
+  var client = new BinaryClient('wss://kasoot.herokuapp.com:9000');
 
   document.querySelector('input[type=file]').onchange = function(event) {
     var file = event.target.files[0];
