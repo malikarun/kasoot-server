d9f3781a91a4e0607350c8b5b25b6f44433db5fc
malikarun <akmalik@ymail.com>
2015-11-15 10:54:36 +1000

    First commit

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 var fs = require('fs');
 var BinaryServer = require('binaryjs').BinaryServer;
 
+var http = require("http");
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 9001;
+
+app.use(express.static(__dirname + "/"));
+
+var server = http.createServer(app);
+server.listen(port);
+
 // Serve client side statically
-var server = BinaryServer({
-  port: 9000
-});
+var binaryServer = BinaryServer({ port: 9000 });
 
 // wait for new user connections
 var currentSong;
-server.on('connection', function(client) {
+binaryServer.on('connection', function(client) {
   if (currentSong && currentSong.length > 0) {
     var song = client.createStream();
     client.send(currentSong);
@@ -22,8 +30,8 @@ server.on('connection', function(client) {
     });
 
     // broadcast to all other clients
-    for (var id in server.clients) {
-      var otherClient = server.clients[id];
+    for (var id in binaryServer.clients) {
+      var otherClient = binaryServer.clients[id];
       if (otherClient !== client) {
         var song = otherClient.createStream(meta);
         stream.pipe(song);
